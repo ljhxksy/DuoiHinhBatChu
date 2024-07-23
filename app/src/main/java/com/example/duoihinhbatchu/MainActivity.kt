ff9c115811aa82cad46e8eed67c6cfd84a9207fb
ljhxksy <132593990+ljhxksy@users.noreply.github.com>
2024-07-23 13:56:59 +0700

    init!!

---
+++ app/src/main/java/com/example/duoihinhbatchu/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.duoihinhbatchu
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.duoihinhbatchu.ui.theme.DuoiHinhBatChuTheme
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.main_layout)
+
+    }
+}
